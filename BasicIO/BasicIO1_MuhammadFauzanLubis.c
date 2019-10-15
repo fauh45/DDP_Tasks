bbd618dc5168d7211445d5b004d82b6a14ea7f4e
Fauzan Lubis <fauzanfauh45@gmail.com>
2019-10-15 21:50:05 +0700

    Add README.md and first task

---
+++ BasicIO/BasicIO1_MuhammadFauzanLubis.c
@@ -0,0 +1,19 @@
+/* Program      : BasicIO1_MuhammadFauzanLubis.c
+Deskripsi       : Program akan mengembalikan value input dengan value dua kali input
+Nama /Author    : Muhammad Fauzan Lubis
+Tanggal/versi   : 15 Oktober 2019/1.0
+Compiler        : gcc (tdm64-1) 5.1.0
+Pertanyaan      : Buatlah program yang menampilkan bilangan yang tepat dua kali lipat dari bilangan yang berasal dari inputan pengguna.
+
+======================================*/
+#include <stdio.h>
+
+int main() {
+    // Variable Declaration
+    int masuk;
+
+    // Process
+    scanf("%d", &masuk);
+    printf("%d\n", masuk*2);
+    
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# DDP Tasks
+DDP or as known as Dasar-dasar pemograman is a class in Bandung State Polytechnic that learns about programming basics
+
+Here is collection of my tasks while doing that class
+
+**fauh45**
